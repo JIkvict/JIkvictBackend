91180ebbe74480c8e349b66db28ca5b5bf2b4b9b
ikvict07 <antongorobec101@gmail.com>
2025-08-02 03:44:21 +0200

    Update objectMapper configuration

--- src/main/kotlin/org/jikvict/jikvictbackend/configuration/JacksonConfiguration.kt
+++ src/main/kotlin/org/jikvict/jikvictbackend/configuration/JacksonConfiguration.kt
@@ -3,29 +3,30 @@ package org.jikvict.jikvictbackend.configuration
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.hibernate.cfg.AvailableSettings
 import org.hibernate.type.format.jackson.JacksonJsonFormatMapper
 import org.jikvict.testing.model.TestSuiteResult
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
+import jakarta.annotation.PostConstruct
 
 @Configuration
-class JacksonConfiguration {
+class JacksonConfiguration(
+    private val objectMapper: ObjectMapper,
+) {
     abstract class TestSuiteResultMixin {
         @Suppress("unused")
         @get:JsonIgnore
         abstract val percentageEarned: Double
     }
 
-    @Bean
-    @Primary
-    fun objectMapper(): ObjectMapper =
-        jacksonObjectMapper()
+    @PostConstruct
+    fun customObjectMapper() {
+        objectMapper
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .addMixIn(TestSuiteResult::class.java, TestSuiteResultMixin::class.java)
+    }
 
     @Bean
     fun jsonFormatMapperCustomizer(objectMapper: ObjectMapper?): HibernatePropertiesCustomizer =
