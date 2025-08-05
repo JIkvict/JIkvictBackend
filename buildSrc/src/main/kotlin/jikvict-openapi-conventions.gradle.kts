0e99ba3564e5845c4444f922cd78502bd5701d31
ikvict07 <antongorobec101@gmail.com>
2025-08-05 21:34:24 +0200

    40s timeout

--- buildSrc/src/main/kotlin/jikvict-openapi-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jikvict-openapi-conventions.gradle.kts
@@ -8,5 +8,5 @@ openApi {
     outputDir.set(file("${layout.buildDirectory.get()}/openapi"))
     outputFileName.set("openapi.json")
     apiDocsUrl.set("http://localhost:8080/v3/api-docs")
-    waitTimeInSeconds.set(10)
+    waitTimeInSeconds.set(40)
 }
