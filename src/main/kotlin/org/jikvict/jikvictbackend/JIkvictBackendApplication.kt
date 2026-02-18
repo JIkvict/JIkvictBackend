f7729897debcc23e58d90a002a7b91206a46eb63
ikvict07 <antongorobec101@gmail.com>
2026-02-18 15:36:02 +0100

    enable method security

--- src/main/kotlin/org/jikvict/jikvictbackend/JIkvictBackendApplication.kt
+++ src/main/kotlin/org/jikvict/jikvictbackend/JIkvictBackendApplication.kt
@@ -2,8 +2,10 @@ package org.jikvict.jikvictbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
+@EnableMethodSecurity
 class JIkvictBackendApplication
 
 fun main(args: Array<String>) {
